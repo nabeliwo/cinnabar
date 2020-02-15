111aa1673705ca233117304be5dae71fa226d7ff
nabeliwo <hiroki.c.watanabe@gmail.com>
2020-02-15 21:52:23 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export const App: FC<{ title: string }> = ({ title }) => (
   <>
     <h1>{title}</h1>
     <Message />
+    {console.log('test')}
     <img src="/assets/img/icon.png" alt="" />
   </>
 )
